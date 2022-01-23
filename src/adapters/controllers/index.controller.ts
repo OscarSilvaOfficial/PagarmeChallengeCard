7d39b1033f64e949b5dd952b019f351488ed4f69
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-23 19:15:49 -0300

    Ajuste no nome da função

--- src/adapters/controllers/index.controller.ts
+++ src/adapters/controllers/index.controller.ts
@@ -3,13 +3,13 @@ import { IHttpServer } from "../../infra/frameworks/web/interfaces/http";
 export const IndexController = (http: IHttpServer) => {
   
   @http.controller('/')
-  class IndexController {
+  class Controller {
     @http.get('/')
     test(@http.response() res: any) {
       res.send('Hello World');
     }
   }
 
-  return IndexController;
+  return Controller;
   
 }
